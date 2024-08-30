d30f84f3448469acdcd75fb670889fed858a9a19
NVan2903 <tnvan2903@gmail.com>
2024-08-30 22:29:18 +0700

    update lab

--- lab_1_1/android/app/src/main/kotlin/flutter/lab_1_1/lab_1_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package flutter.lab_1_1.lab_1_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lab_1_2/android/app/src/main/kotlin/com/example/lab_1_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab_1_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
